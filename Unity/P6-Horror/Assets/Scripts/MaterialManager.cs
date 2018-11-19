53b5384880533d45463798e5812b70ec52527b91
Daniel-Stremmelaar <33859025+Daniel-Stremmelaar@users.noreply.github.com>
2018-11-19 10:56:46 +0100

    Material Changer Script
    Basic script to change materials is working.

---
+++ Unity/P6-Horror/Assets/Scripts/MaterialManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialManager : MonoBehaviour {
+
+    public Material current;
+    public Material past;
+    public Material present;
+
+	// Use this for initialization
+	void Start () {
+        current = gameObject.GetComponent<Renderer>().material;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Jump"))
+        {
+            ChangeMaterial();
+        }
+    }
+
+    public void ChangeMaterial()
+    {
+        //check current material
+        if (current == past)
+        {
+            //change material to modern day
+            current = present;
+        }
+        else
+        {
+            //change material to old house
+            current = past;
+        }
+        //set material to object
+        gameObject.GetComponent<Renderer>().material = current;
+    }
+}
