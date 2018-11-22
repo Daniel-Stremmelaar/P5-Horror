5675ae32c0d6b1a5ec85fd8d22d29871b95164c0
Daniel-Stremmelaar <33859025+Daniel-Stremmelaar@users.noreply.github.com>
2018-11-22 15:43:18 +0100

    End of day push
    Added fonts, started work on getting notebooks working.

---
+++ Unity/P6-Horror/Assets/Scripts/NotebookManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotebookManager : MonoBehaviour {
+
+    public List<string> notebooks = new List<string>();
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SetText(int i)
+    {
+
+    }
+}
