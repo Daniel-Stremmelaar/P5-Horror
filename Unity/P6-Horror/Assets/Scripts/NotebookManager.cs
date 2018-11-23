e495a23abe3b75cb34f3a9645c9e1d56ca3bba79
Daniel-Stremmelaar <33859025+Daniel-Stremmelaar@users.noreply.github.com>
2018-11-23 13:38:36 +0100

    Notebook Manager
    Notebook manager werkt. Moet nog worden geüpdate voor interactie met objecten.
    Kan nu ook fonts veranderen.

--- Unity/P6-Horror/Assets/Scripts/NotebookManager.cs
+++ Unity/P6-Horror/Assets/Scripts/NotebookManager.cs
@@ -1,23 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NotebookManager : MonoBehaviour {
-
-    public List<string> notebooks = new List<string>();
+    [TextArea] public List<string> notebooks = new List<string>();
+    public List<Font> fonts = new List<Font>();
+    public Text text;
+    private int i;
+    private int n;
 
 	// Use this for initialization
 	void Start () {
-		
+        i = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (Input.GetButtonDown("Jump"))
+        {
+            i++;
+            if(i > notebooks.Count-1)
+            {
+                i = 0;
+            }
+            SetText(i);
+        }
+        if (Input.GetButtonDown("Submit"))
+        {
+            n++;
+            if(n > fonts.Count - 1)
+            {
+                n = 0;
+            }
+            SetFont(n);
+        }
 	}
 
     public void SetText(int i)
     {
+        text.text = notebooks[i];
+    }
 
+    public void SetFont(int n)
+    {
+        text.font = fonts[n];
     }
 }
