02895d1771375fa11e0af2229a235f8d701830d2
Daniel-Stremmelaar <33859025+Daniel-Stremmelaar@users.noreply.github.com>
2018-11-27 14:58:01 +0100

    Menus done
    Main menu, options menu, pause menu and in game sound menu are all ready for use.

---
+++ Unity/P6-Horror/Assets/Scripts/Menu.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+    public Button menu;
+    public Button game;
+    public Button options;
+    public Button exit;
+    public GameObject pauseMenu;
+    public GameObject soundMenu;
+
+	// Use this for initialization
+	void Start () {
+        if(menu != null)
+        {
+            menu.onClick.AddListener(delegate { SceneManager.LoadScene(0); });
+        }
+        
+        if(game != null)
+        {
+            if(SceneManager.GetActiveScene().buildIndex == 1)
+            {
+                game.onClick.AddListener(Continue);
+            }
+            else
+            {
+                game.onClick.AddListener(delegate { SceneManager.LoadScene(1); });
+            }
+        }
+        
+        if(options != null)
+        {
+            if (SceneManager.GetActiveScene().buildIndex == 1)
+            {
+                options.onClick.AddListener(SoundOptions);
+            }
+            else
+            {
+                options.onClick.AddListener(delegate { SceneManager.LoadScene(2); });
+            }
+        }
+        
+        if(exit != null)
+        {
+            exit.onClick.AddListener(delegate { Application.Quit(); });
+        }
+    }
+
+    void Continue()
+    {
+        Time.timeScale = 1;
+        soundMenu.SetActive(false);
+        pauseMenu.SetActive(false);
+    }
+
+    void SoundOptions()
+    {
+        soundMenu.SetActive(true);
+        pauseMenu.SetActive(false);
+    }
+}
