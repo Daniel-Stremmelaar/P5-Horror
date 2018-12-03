b74e33dead8d5b211d23689c2e0e4063e919c122
Daniel-Stremmelaar <33859025+Daniel-Stremmelaar@users.noreply.github.com>
2018-12-03 08:59:43 +0100

    Interact push
    Interact base script and drawer.

---
+++ Unity/P6-Horror/Assets/Scripts/Interactables/Drawer.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drawer : Interactable {
+
+    public Vector3 closed;
+    public Vector3 open;
+    public float speed;
+    private bool moving;
+    private bool opened;
+
+    public override void Interact()
+    {
+        base.Interact();
+        moving = true;
+    }
+
+    private void Start()
+    {
+        closed = transform.position;
+        open = transform.position;
+        open.z += 1;
+        opened = false;
+    }
+
+    private void FixedUpdate()
+    {
+        if (moving == true)
+        {
+            if(opened == false)
+            {
+                transform.Translate(transform.forward * Time.deltaTime * speed);
+                if(transform.position == open)
+                {
+                    opened = true;
+                    moving = false;
+                }
+            }
+            else
+            {
+                transform.Translate(-transform.forward * Time.deltaTime * speed);
+                if (transform.position == closed)
+                {
+                    opened = false;
+                    moving = false;
+                }
+            }
+        }
+    }
+}
---
+++ Unity/P6-Horror/Assets/Scripts/PlayerInteraction.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour {
+
+    private RaycastHit hit;
+    public int interactDistance;
+    public GameObject interactDisplay;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.DrawRay(transform.position, transform.forward * interactDistance);
+		if(Physics.Raycast(transform.position, transform.forward, out hit, interactDistance))
+        {
+            if(hit.transform.gameObject.tag == "Interactable")
+            {
+                interactDisplay.SetActive(true);
+                if (Input.GetKeyDown("e"))
+                {
+                    hit.transform.gameObject.GetComponent<Interactable>().Interact();
+                }
+            }
+            else
+            {
+                interactDisplay.SetActive(false);
+            }
+        }
+	}
+}
