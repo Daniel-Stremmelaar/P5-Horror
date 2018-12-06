a7801a30fb4f089ab5b5ae94e47b5b7e8dd9fd92
Daniel-Stremmelaar <33859025+Daniel-Stremmelaar@users.noreply.github.com>
2018-12-06 11:44:58 +0100

    Enemy AI
    Working on AI:
    - Detection of player

--- Unity/P6-Horror/Assets/Scripts/EnemyBehavior.cs
+++ Unity/P6-Horror/Assets/Scripts/EnemyBehavior.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class EnemyBehavior : MonoBehaviour {
 
+    public float detectionDistance;
+    public float detectionAngle;
+    public Transform player;
+    private Vector3 playerDirection;
+    private float playerDistance;
+    private float playerAngle;
+
+    public Transform target;
+    public List<Transform> targetList = new List<Transform>();
+
     private enum action { wander, scan, hunt, search };
     private action current;
 
@@ -33,4 +43,11 @@ public class EnemyBehavior : MonoBehaviour {
                 break;
         }
 	}
+
+    private void Vision()
+    {
+        playerDirection = player.position - transform.position;
+        playerDistance = Mathf.Sqrt(Mathf.Pow((player.position.z - transform.position.z), 2) + Mathf.Pow((player.position.x - transform.position.x), 2));
+        playerAngle = Vector3.Angle(playerDirection, transform.forward);
+    }
 }
