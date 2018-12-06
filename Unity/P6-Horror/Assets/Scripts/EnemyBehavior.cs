1fe1772a59b7c640c1d65c81a52cf85c16162657
Daniel-Stremmelaar <33859025+Daniel-Stremmelaar@users.noreply.github.com>
2018-12-06 10:08:45 +0100

    AI notes and start of script
    AI notes and start of AI script.

---
+++ Unity/P6-Horror/Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour {
+
+    private enum action { wander, scan, hunt, search };
+    private action current;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch (current)
+        {
+            case action.wander:
+                //behavior here
+                break;
+
+            case action.scan:
+                //behavior here
+                break;
+
+            case action.hunt:
+                //behavior here
+                break;
+
+            case action.search:
+                //behavior here
+                break;
+        }
+	}
+}
