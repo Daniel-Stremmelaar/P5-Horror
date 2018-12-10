f9716cea0f03dffda06ab41e65c3292e4ec5a5d7
JorritKornelis <33859036+JorritKornelis@users.noreply.github.com>
2018-12-10 14:56:33 +0100

    pils
    pilser beer

--- Unity/P6-Horror/Assets/Scripts/UIManager.cs
+++ Unity/P6-Horror/Assets/Scripts/UIManager.cs
@@ -6,19 +6,24 @@ using UnityEngine.UI;
 public class UIManager : MonoBehaviour
 {
     [Header("DeathScreen")]
-    public GameObject fadeToBlack;
+    public GameObject fadeToBlackOBJ;
+    public Color fateToBlackColor;
     public float FadeToBlackAmount;
     public GameObject deathScreenOBJ;
+    bool deathBool;
+    public Animator deathPopUp;
 
     [Header("PauseMeneu")]
     public GameObject pauseMenu;
 
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start()
     {
         deathScreenOBJ.SetActive(false);
-	}
+        fateToBlackColor.a = 0f;
+        DeathUI();
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -27,14 +32,25 @@ public class UIManager : MonoBehaviour
             Time.timeScale = 0;
             pauseMenu.SetActive(true);
         }
-	}
+        
+        if (deathBool == true)
+        {
+            FateToBlack();
+        }
+    }
 
     public void DeathUI()
     {
-        //fadeToBlack.GetComponent<Image>().color.a += FadeToBlackAmount * Time.deltaTime;
-        if (FadeToBlackAmount == 225f)
+        deathBool = true;
+    }
+    public void FateToBlack()
+    {
+        fadeToBlackOBJ.GetComponent<Image>().color = fateToBlackColor;
+        fateToBlackColor.a += FadeToBlackAmount * Time.deltaTime;
+        if (fateToBlackColor.a >= 1.2f)
         {
             deathScreenOBJ.SetActive(true);
+            deathPopUp.Play("TextAnimation");
         }
     }
 }
