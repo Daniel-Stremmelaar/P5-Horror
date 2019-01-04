95110e4d5cc750cc5ed6c7036cbb8d17957de1db
JorritKornelis <33859036+JorritKornelis@users.noreply.github.com>
2019-01-04 16:52:30 +0100

    ss
    ss

---
+++ Unity/P6-Horror/Assets/Scripts/NoteBook.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteBook : MonoBehaviour
+{
+    public List<string> mainObjective = new List<string>();
+    public List<string> objectiveSteps = new List<string>();
+
+}
