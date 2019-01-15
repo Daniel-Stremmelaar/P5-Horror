66746aee0f3829d99101c33878f7bdef87959801
Remasu <stefanherder2001@gmail.com>
2019-01-15 14:04:56 +0100

    Added shader management scripts
    They are made for changing the shader values when the player enters the specified triggers.

---
+++ Unity/P6-Horror/Assets/Scripts/ShaderControllers/Room.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour {
+
+    public MeshRenderer[] meshRenderers;
+    public float transitionValue;
+    public float targetValue;
+    public bool oldTimes;
+
+    public float transitionSpeed = 1;
+
+    public RoomManager roomManager;
+
+    public void Switcheroo()
+    {
+        if (oldTimes)
+        {
+            BecomeOld();
+        }
+        else
+        {
+            BecomeNew();
+        }
+    }
+
+    public void Update()
+    {
+        if(transitionValue == targetValue)
+        {
+            return;
+        }
+        else
+        {
+            if (transitionValue > targetValue)
+            {
+                transitionValue -= transitionSpeed*Time.deltaTime;
+            }
+            else
+            {
+                if (transitionValue < targetValue)
+                {
+                    transitionValue += transitionSpeed * Time.deltaTime;
+                }
+            }
+        }
+
+        for (int i = 0; i < meshRenderers.Length; i++)
+        {
+            meshRenderers[i].material.SetFloat("_Progress", transitionValue);
+        }
+    }
+
+    public void BecomeOld()
+    {
+        oldTimes = true;
+        targetValue = 2;
+    }
+
+    public void BecomeNew()
+    {
+        oldTimes = false;
+        targetValue = 0;
+    }
+
+    public void OnTriggerEnter(Collider col)
+    {
+        if(col.tag == "Player")
+        {
+            Switcheroo();
+            gameObject.GetComponent<BoxCollider>().enabled = false;
+            roomManager.RemoveFromList();
+        }
+    }
+}
---
+++ Unity/P6-Horror/Assets/Scripts/ShaderControllers/RoomManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomManager : MonoBehaviour {
+
+    [Tooltip("These need to be in the correct order")]
+    public List<Room> rooms;
+
+    public void Start()
+    {
+        rooms[0].enabled = true;
+    }
+
+    public void RemoveFromList()
+    {
+        rooms.Remove(rooms[0]);
+        for (int i = 1; i < rooms.Capacity; i++)
+        {
+            rooms[i--] = rooms[i];
+        }
+        rooms[0].enabled = true;
+    }
+}
