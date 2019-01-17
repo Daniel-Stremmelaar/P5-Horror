e0b4912f4243d704555356c1b5ea72137a28e67e
Daniel-Stremmelaar <33859025+Daniel-Stremmelaar@users.noreply.github.com>
2019-01-17 10:33:31 +0100

    FrontDoorEnding script
    win script

---
+++ Unity/P6-Horror/Assets/Scripts/Interactables/FrontDoor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FrontDoor : Door {
+
+    public Text gameEnd;
+    public UIManager ui;
+
+    public override void Interact()
+    {
+        if (unlocked == true)
+        {
+            source.PlayOneShot(clip);
+            ui.DeathUI();
+        }
+    }
+}
